3df83a8f67cb95e66b8d2e83d86f774a6f055a54
lovelynoel <shduf123@naver.com>
2022-03-03 12:39:12 +0900

    first commit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,47 @@
-const $container=$('.gallery'); // 이미지들을 감싸는 갤러리
-const $loadMoreBtn=$('.load-more'); //더보기 버튼
-let $addItemCount=8;  //item 8개 보이기 클릭할때마다 나오는 갯수
-let $added=0; //처음보이는 것 리스트 항목 모두 로드하면 버튼이 사라지게 할 용도
-let $allData=[];
+$(function(){
+    const $container=$('.gallery');//이미지들을 감싸는 갤러리
+    const $loadMoreBtn=$('.load-more');//더보기 버튼
+    let $addItemCount=8; //클릭할떄마다 나오는 갯수
+    let $added=0;//처음보이는 것 리스트 항목 모두 로드하면 버튼
+    let $allData=[];
+    
+    $.getJSON("./data/content.json", function(data){
+        intGallery(data);
+    });
+    function intGallery(data){
+        $allData=data;
+        addItem();
+       //console.log($allData);
+       $loadMoreBtn.click(function(){
+            addItem();
+       });
+    }
+    function addItem(){
+        let elements=[];
+        let sliceDate;
+        sliceDate=$allData.slice($added, $added += $addItemCount);
+        $.each(sliceDate, function(idx, item){
+            let itemHTML=
+            '<li class="gallery-item">'+
+                '<a href="'+item.images.large+'">'+
+                    '<figure>'+
+                        '<img src="'+item.images.thumb+'" alt="'+item.title+'">'+
+                        '<figcaption>'+item.title+'</figcaption>'+
+                    '</figure>'+
+                '</a>'+
+            '</li>';
+            elements.push($(itemHTML).get(0));
+        });
+        $container.append(elements);
+        $added += sliceDate.length;
 
-$.getJSON('./data/content.json', function(data){
-
+        if($added < $allData.length){
+            $loadMoreBtn.show()
+        }else{
+            $loadMoreBtn.hide()
+        }
+        $container.imagesLoaded( function() {
+            // images have loaded
+          });
+    }
 });
-function initGallery(data){
-    $allData=Data;
-    console.log($allData);
-}
